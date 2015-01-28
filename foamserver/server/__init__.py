f0a89f0170439f345f6a0dcad08ed993c589532d
Martin Ortbauer <mortbauer@gmail.com>
2015-01-28 17:42:40 +0100

    added try except around processing and add to failed queue if this fails

--- foamserver/server/__init__.py
+++ foamserver/server/__init__.py
@@ -226,6 +226,7 @@ class FoamPostProcessorHDF5(BaseApp):
     def __init__(self,**kwargs):
         super(FoamPostProcessorHDF5,self).__init__(**kwargs)
         self.currently_processing = self.make_redis_name('currently_processing')
+        self._failed = self.make_redis_name('failed')
         self.conn = pymongo.MongoClient()
         self.db = self.conn['foamserver_postpro']
 
@@ -235,8 +236,24 @@ class FoamPostProcessorHDF5(BaseApp):
             if d_id is None:
                 gevent.sleep(1.)
             else:
-                if self.redis.sadd(self.currently_processing,d_id):
+                if self.redis.sadd(self.currently_processing,d_id) and not \
+                        self.sismember(self._failed,d_id):
                     self.process_doc(d_id)
+                    self.redis.srem(self.currently_processing,d_id)
+
+
+    def process_failed_loop(self,failed=''):
+        while not self._shutdown:
+            d_id = self.redis.spop('::'.join((self._failed,str(failed))))
+            if d_id is None:
+                break
+            else:
+                if self.redis.sadd(self.currently_processing,d_id):
+                    if not self.redis.hget(d_id,'n_curr'):
+                        self.redis.hdel(d_id,'header')
+                        self.process_doc(d_id)
+                    else:
+                        logger.error('i\'m scared to process %s since there is already data',d_id)
                     self.redis.srem(self.currently_processing,d_id)
 
     def process_doc(self,d_id):
@@ -272,18 +289,25 @@ class FoamPostProcessorHDF5(BaseApp):
                 payload = loads(payload_msg)
                 group = self.datastore[h5_id]
                 t0 = time.time()
-                processor(group,d_id,meta,payload)
-                self.redis.hincrby(d_id,'next')
-                logger.debug('processed %s::%s in %s sec',d_id,n,time.time()-t0)
-                # store into mongo
-                d_doc = d._asdict()
-                d_doc['n'] = n
-                self.db.archive.insert(
-                    {'_id':d_doc,'meta':meta,'payload':payload_msg})
-                self.redis.hdel(msgs_id,n)
-                self.datastore.flush()
+                try:
+                    processor(group,d_id,meta,payload)
+                except Exception as e:
+                    logger.critical('failed to process: %s::%s',d_id,n)
+                    n_failed = self.redis.hincrby(d_id,'failed')
+                    self.redis.sadd('::'.join((self._failed,n_failed)),d_id)
+                    break
+                else:
+                    self.redis.hincrby(d_id,'next')
+                    logger.debug('processed %s::%s in %s sec',d_id,n,time.time()-t0)
+                    # store into mongo
+                    d_doc = d._asdict()
+                    d_doc['n'] = n
+                    self.db.archive.insert(
+                        {'_id':d_doc,'meta':meta,'payload':payload_msg})
+                    self.redis.hdel(msgs_id,n)
+                    self.datastore.flush()
 
-    def start(self):
+    def start(self,failed=0):
         super(FoamPostProcessorHDF5,self).start()
         logger.info("started server")
         filename = self.conf.get('datafile','%s.h5'%self.name)
@@ -300,7 +324,10 @@ class FoamPostProcessorHDF5(BaseApp):
             #gevent.spawn(self.process_loop),
         #])
         #self.datastore.atomic = True
-        self.process_loop()
+        if failed is None:
+            self.process_loop()
+        else:
+            self.process_failed_loop(failed)
         self.datastore.close()
 
     def process_log(self,group,d_id,meta,lines):
@@ -487,7 +514,7 @@ class FoamPostProcessorHDF5(BaseApp):
                         cols[j][i,k] = float(x)
         else:
             for i,line in enumerate(lines):
-                fields = line.strip('\n#').split('\t',2)
+                fields = line.strip('').split('\t')
                 timeset[i] = float(fields[0])
                 for j,x in enumerate(fields[1:]):
                     cols[j][i] = float(x)
@@ -536,337 +563,7 @@ class FoamPostProcessorHDF5(BaseApp):
                     header.append('/'.join((key,minor)))
         else:
             ndim = 1
-            header = third_line.strip('\n# ').split('\t',2)[1:]
-        return header,ndim
-
-class FoamPostProcessor(BaseApp):
-    NAME = 'postpro'
-    CONF = {
-        'datafile':'foamserver_data.h5',
-    }
-    doc = collections.namedtuple(
-        'doc',['host','project','uuid','type','path','initially'])
-
-    var_row = [
-        ('solver','S12'),('initial','f8'),('final','f8'),('n_lin_iter','i4'),
-        ('n_iter','i8'),('n_outer_corr','i8'),('n_ortho','i8')]
-
-    tsce_row = [
-        ('sum local','f8'),('global','f8'),('cumulative','f8'),
-        ('n_iter','i8'),('n_outer_corr','i8')]
-
-    main_row = [
-        ('time','f8'),('execution_time','f8'),('clock_time','f8'),
-        ('convergence','S40'),('delta_t','f8'),('cfl_mean','f8'),
-        ('cfl_max','f8'),('n_iter','i8'),('n_outer_corr','i8')]
-
-    reread_row = [('n_iter','i8'),('name','S20'),('path','S150')]
-
-    def __init__(self,**kwargs):
-        super(FoamPostProcessor,self).__init__(**kwargs)
-        self.currently_processing = self.make_redis_name('currently_processing')
-
-    def process_loop(self):
-        while not self._shutdown:
-            d_id = self.redis.lpop(self.raw_queue)
-            if d_id is None:
-                gevent.sleep(1.)
-            else:
-                if self.redis.sadd(self.currently_processing,d_id):
-                    self.process_doc(d_id)
-                    self.redis.srem(self.currently_processing,d_id)
-
-    def process_doc(self,d_id):
-        d = self.doc(*d_id[5:].split('::'))
-        h5_id = d_id[5:].replace('::','/')
-        if d.type == 'system':
-            return
-        processor = getattr(self,'process_%s'%d.type)
-        #print('poping of %s'%d_id)
-        if not h5_id in self.datastore:
-            group = self.datastore.create_group(h5_id)
-            # create also a shortcut to the new data
-            # project wide
-            latest = '/'.join((d.host,d.project,'latest'))
-            if latest in self.datastore:
-                del self.datastore[latest]
-            self.datastore[latest] = self.datastore['/'.join((d.host,d.project,d.uuid))]
-            # doc wide
-            latest = '/'.join((group.parent.name,'latest'))
-            if latest in self.datastore:
-                del self.datastore[latest]
-            self.datastore[latest] = self.datastore[group.name]
-        else:
-            group = self.datastore[h5_id]
-        msgs_id = '::'.join((d_id,'msgs'))
-        while not self._shutdown:
-            n = self.redis.hget(d_id,'next')
-            if n is None:
-                n = 0
-            msg = self.redis.hget(msgs_id,n)
-            if not msg:
-                break
-            else:
-                meta,payload_msg = loads(msg)
-                payload = loads(payload_msg)
-                group = self.datastore[h5_id]
-                t0 = time.time()
-                processor(group,d_id,meta,payload)
-                self.redis.hincrby(d_id,'next')
-                logger.debug('processed %s::%s in %s sec',d_id,n,time.time()-t0)
-                self.redis.hdel(msgs_id,n)
-                self.datastore.flush()
-
-    def start(self):
-        super(FoamPostProcessor,self).start()
-        logger.info("started server")
-        filename = self.conf.get('datafile','%s.h5'%self.name)
-        try:
-            #self.datastore = h5py.File(
-                #filename,'r+',driver='mpio',comm=MPI.COMM_WORLD)
-            self.datastore = h5py.File(filename,'r+')
-        except IOError:
-            #self.datastore = h5py.File(
-                #filename,'w',driver='mpio',comm=MPI.COMM_WORLD)
-            self.datastore = h5py.File(filename,'w')
-            logger.debug('creating file "%s"',filename)
-        #gevent.joinall(
-            #gevent.spawn(self.process_loop),
-        #])
-        #self.datastore.atomic = True
-        self.process_loop()
-        self.datastore.close()
-
-    def process_log(self,group,d_id,meta,lines):
-        self.redis.hsetnx(d_id,'n_runs',0)
-        self.redis.hsetnx(d_id,'n_iter',0)
-        self.redis.hsetnx(d_id,'n_outer_corr',0)
-        self.redis.hsetnx(d_id,'n_corr',0)
-        self.redis.hsetnx(d_id,'n_orthogonality',0)
-        self.redis.hsetnx(d_id,'start_time',0)
-        self.redis.hsetnx(d_id,'executable','')
-        self.redis.hsetnx(d_id,'cfl_mean',0)
-        self.redis.hsetnx(d_id,'cfl_max',0)
-        self.redis.hsetnx(d_id,'deltaT',0)
-        n_runs = self.redis.hget(d_id,'n_runs')
-        executable = self.redis.hget(d_id,'executable')
-        while len(lines):
-            if lines[0].startswith('Build  :'):
-                if 'run%s'%n_runs not in group:
-                    sg = group.create_group('run%s'%n_runs)
-                else:
-                    sg = group['run%s'%n_runs]
-                executable = self.process_log_header(sg,d_id,meta,lines)
-            elif 'simple' in executable or 'pimple' in executable:
-                self.process_log_imple(group['run%s'%n_runs],d_id,meta,lines)
-                executable = self.redis.hget(d_id,'executable')
-                n_runs = self.redis.hget(d_id,'n_runs')
-            else:
-                lines.pop(0)
-                if len(executable):
-                    logger.error('unknown exec: %s',executable)
-
-    def process_log_header(self,group,d_id,meta,lines):
-        # get the executable info
-        d = {}
-        for i in range(8):
-            line = lines.pop(0)
-            key,value = line.strip().split(':',1)
-            d[key.strip()] = value.strip()
-        # get the host computers
-        if int(value) > 1:
-            key = lines.pop(0).strip(' :\n')
-            lines.pop(0)
-            lines.pop(0)
-            slaves = []
-            for i in range(int(value)-1):
-                slaves.append(lines.pop(0).strip(' \n"'))
-            lines.pop(0)
-            d[key] = slaves
-        self.redis.hset(d_id,'executable_info',dumps(d))
-        self.redis.hset(d_id,'executable',d['Exec'].lower())
-        # write the info as attrs
-        for k in d:
-            group.attrs[k] = d[k]
-        return d['Exec'].lower()
-
-    def process_log_imple(self,group,d_id,meta,lines):
-        d = {}
-        tsceinfo = []
-        maininfo = []
-        reread = []
-        n_iter = int(self.redis.hget(d_id,'n_iter'))
-        n_outer_corr = int(self.redis.hget(d_id,'n_outer_corr'))
-        n_corr = int(self.redis.hget(d_id,'n_corr'))
-        n_orthogonality = int(self.redis.hget(d_id,'n_orthogonality'))
-        start_time = float(self.redis.hget(d_id,'start_time'))
-        convergence_info = self.redis.hget(d_id,'convergence_info')
-        cfl_mean = self.redis.hget(d_id,'cfl_mean')
-        cfl_max = self.redis.hget(d_id,'cfl_max')
-        deltaT = self.redis.hget(d_id,'deltaT')
-        while len(lines):
-            line = lines.pop(0).strip()
-            if line.startswith('Time ='):
-                key, value = line.split(' = ')
-                start_time = float(value)
-            if 'Solving for ' in line:
-                key,dd,start = proc_line(
-                    line,n_iter,n_outer_corr,n_orthogonality)
-                if not key in d:
-                    d[key] = []
-                d[key].append(dd)
-                while len(lines):
-                    if not lines[0].startswith(start):
-                        n_orthogonality = 0
-                        break
-                    n_orthogonality += 1
-                    key,dd,start = proc_line(
-                        lines.pop(0),n_iter,n_outer_corr,n_orthogonality)
-                    d[key].append(dd)
-            elif line.startswith('Courant Number mean:'):
-                fields = line.split(': ')
-                cfl_mean = float(fields[1].split()[0])
-                cfl_max = float(fields[2])
-            elif line.startswith('deltaT ='):
-                deltaT = float(line.split(' = ')[1])
-            elif line.startswith('time step continuity errors'):
-                key, rest = line.split(':')
-                fields = rest.split(',')
-                tsceinfo.append((float(fields[0].split('=')[1].strip()),
-                      float(fields[1].split('=')[1].strip()),
-                      float(fields[2].split('=')[1].strip()),n_iter,n_outer_corr))
-            elif line.startswith('PIMPLE: iteration'):
-                n_outer_corr += 1
-            elif line.startswith('PIMPLE: '):
-                convergence_info = line.split(':')[1].strip()
-            elif 'Re-reading object' in line:
-                name,filepath = line[18:].split(' from file ')
-                reread.append((n_iter,name,filepath.strip('"')))
-            elif line.startswith('ExecutionTime'):
-                fields = line.split('  ')
-                maininfo.append((
-                    start_time,
-                    float(fields[0].split(' = ')[1][:-2]),
-                    float(fields[0].split(' = ')[1][:-2]),
-                    convergence_info,deltaT,cfl_mean,cfl_max,n_iter,n_outer_corr,
-                ))
-                n_outer_corr = 0
-                n_iter +=1
-            elif line.startswith('End'):
-                self.redis.hset(d_id,'executable','')
-                self.redis.hincrby(d_id,'n_runs')
-                n_iter = 0
-                n_outer_corr = 0
-                break
-
-        # update the generic keys
-        for key in d:
-            n = len(d[key])
-            if not key in group:
-                ds = group.create_dataset(
-                    key,dtype=self.var_row,shape=(n,),
-                    chunks=True,maxshape=(None,))
-            else:
-                ds = group[key]
-                ds.resize((ds.shape[0]+n,))
-            ds[-n:] = d[key]
-        # update tsce and main
-        for key,dtype,val in (
-            ('main',self.main_row,maininfo),
-            ('reread',self.reread_row,reread),
-            ('tsc',self.tsce_row,tsceinfo)):
-            n = len(val)
-            if n > 0:
-                if not key in group:
-                    ds = group.create_dataset(
-                        key,(n,),dtype,chunks=True,maxshape=(None,))
-                else:
-                    ds = group[key]
-                    ds.resize((ds.shape[0]+n,))
-                ds[-n:] = val
-
-        # save the state
-        self.redis.hset(d_id,'n_iter',n_iter)
-        self.redis.hset(d_id,'n_outer_corr',n_outer_corr)
-        self.redis.hset(d_id,'n_orthogonality',n_orthogonality)
-        self.redis.hset(d_id,'start_time',start_time)
-        self.redis.hset(d_id,'convergence_info',convergence_info)
-        self.redis.hset(d_id,'cfl_mean',cfl_mean)
-        self.redis.hset(d_id,'cfl_max',cfl_max)
-        self.redis.hset(d_id,'deltaT',deltaT)
-
-    def process_system(self,group,d_id,meta,lines):
-        pass
-
-    def process_dat(self,group,d_id,meta,lines):
-        header,ndim = self.process_dat_collective(group,d_id,meta,lines)
-        # create temporary numpy arrays since the are much faster then hdf5
-        # access
-        n_curr_msg = self.redis.hget(d_id,'n_curr')
-        n_curr = 0 if n_curr_msg is None else int(n_curr_msg)
-        size = meta['n_max']-n_curr-2
-        shape = (size,) if ndim == 1 else (size,3)
-        timeset = numpy.empty((size,))
-        cols = [numpy.empty(shape) for x in header]
-        if ndim == 2:
-            for i,line in enumerate(lines):
-                time,fields = line.strip().split('\t')
-                timeset[i] = float(time)
-                for j,main_field in enumerate(fields.split(') ')):
-                    for k,x in enumerate(main_field.strip('()').split()):
-                        cols[j][i,k] = float(x)
-        else:
-            for i,line in enumerate(lines):
-                fields = line.strip('\n#').split('\t',2)
-                timeset[i] = float(fields[0])
-                for j,x in enumerate(fields[1:]):
-                    cols[j][i] = float(x)
-        self.redis.hincrby(d_id,'n_curr',i+1)
-        # transfer the data from our temporary numpy arrays to hdf5
-        for i,x in enumerate(header):
-            group[x][-size:] = cols[i]
-        group['Time'][-size:] = timeset
-
-    def process_dat_collective(self,group,d_id,meta,lines):
-        # take the header from redis if there is
-        header_msg = self.redis.hget(d_id,'header')
-        if header_msg is None:
-            header,ndim = self.process_dat_header(lines)
-            self.redis.hset(d_id,'header',dumps(header))
-            self.redis.hset(d_id,'ndim',ndim)
-        else:
-            header = loads(header_msg)
-            ndim = int(self.redis.hget(d_id,'ndim'))
-        # create the groups/dataset in the hdf5 if not there otherwise resize
-        n_max = meta['n_max']-2
-        shape = (n_max,) if ndim == 1 else (n_max,3)
-        mshape = (None,) if ndim == 1 else (None,3)
-        for key in header:
-            if key in group:
-                group[key].resize(shape)
-            else:
-                group.create_dataset(key,shape,'f8',chunks=True,maxshape=mshape)
-        if 'Time' in group:
-            group['Time'].resize((n_max,))
-        else:
-            group.create_dataset('Time',(n_max,),'f8',chunks=True,maxshape=(None,))
-        return header,ndim
-
-    def process_dat_header(self,lines):
-        first_line = lines.pop(0)
-        second_line = lines.pop(0)
-        third_line = lines.pop(0)
-        if 'Forces' in first_line:
-            ndim = 2
-            header = []
-            comment,time,fields = third_line.split(' ',2)
-            for main_field in fields.strip().split(') '):
-                key,minor_fields = main_field.split('(')
-                for minor in minor_fields.split():
-                    header.append('/'.join((key,minor)))
-        else:
-            ndim = 1
-            header = third_line.strip('\n# ').split('\t',2)[1:]
+            header = third_line.strip('\n# ').split('\t')[1:]
         return header,ndim
 
 
@@ -945,13 +642,14 @@ def server(debug=False,loglevel='error'):
 @click.option('--debug',is_flag=True)
 @click.option('--loglevel',default='error')
 @click.option('--profile',is_flag=True)
-def processor(debug,loglevel,profile):
+@click.option('--failed')
+def processor(debug,loglevel,profile,failed):
     server = FoamPostProcessorHDF5(debug=debug,loglevel=loglevel)
     if profile:
         import cProfile
         import pstats
-        cProfile.runctx('server.start()',None,locals(),'restats')
+        cProfile.runctx('server.start(failed=%s)'%failed,None,locals(),'restats')
         p = pstats.Stats('restats')
         p.strip_dirs().sort_stats('cumulative').print_stats(20)
     else:
-        server.start()
+        server.start(failed=failed)
